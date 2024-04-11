1d3e2098b0266d3493ad9ee102a46edcd59c0b17
ehs208 <ehs208@naver.com>
2024-04-12 00:21:26 +0900

    [feat] #12 관리자 관리 / 관리자 목록 페이지 구현

---
+++ types/AdminManageColumns.ts
@@ -0,0 +1,27 @@
+export const COLUMNS = [
+  {
+    Header: "번호",
+    accessor: "index",
+  },
+  {
+    Header: "이메일",
+    accessor: "member_email",
+  },
+  {
+    Header: "이름",
+    accessor: "member_name",
+  },
+  {
+    Header: "닉네임",
+    accessor: "member_nickname",
+  },
+  {
+    Header: "권한",
+    accessor: "member_Role",
+  },
+  {
+    Header: "가입일",
+    accessor: "member_created_date",
+  },
+
+];
