2f10425d57aa7a4435ccfac96fb0523b9f53f7e3
hyeonjin <11ghyeonjin@gachon.ac.kr>
2024-04-23 01:22:39 +0900

    [hotfix] #54 cors 관련 설정
    next.config를 수정하였습니다.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
