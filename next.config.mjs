0f09f549fcf1873a6b94fb2948dd8d7b8edf698e
hyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2024-04-23 01:27:35 +0900

    Revert "[hotfix] cors 관련 설정"

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
