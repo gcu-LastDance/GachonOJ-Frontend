fde729d067d9058187df57b526778f8743235ff7
EunHyunsu <ehs208@naver.com>
2024-04-26 21:36:41 +0900

    [feat] #86 교수 리스트 조회 화면 구현
    교수 및 학생 조회 API 통합 예정입니다.

--- api/adminUserAPI.ts
+++ api/adminUserAPI.ts
@@ -4,11 +4,23 @@ import { userFormData } from "@/types/admin/user";
 
 export const userListAPI = async () => {
   try {
-    const memberRole = "학생";
-    const url = `${process.env.NEXT_PUBLIC_M09_URL}?memberRole=${encodeURIComponent(memberRole)}`;
+    const url: string = window.location.href;
+    let memberRole: string;
 
-    const response = await instanceAuth.get(url);
-    
+    if (url.includes("user-manage")) {
+        memberRole = "학생"; // 학생으로 설정
+    } else if (url.includes("professor-manage")) {
+        memberRole = "교수"; // 교수로 설정
+    } else {
+        // 다른 경우에 대한 기본값 설정 (예: 기본값을 학생으로 설정)
+        memberRole = "학생";
+    }
+
+    const encodedRole: string = encodeURIComponent(memberRole);
+    const apiUrl: string = `${process.env.NEXT_PUBLIC_M09_URL}?memberRole=${encodedRole}`;
+
+    const response = await instanceAuth.get(apiUrl);
+
     return response.data;
   } catch (error) {
     throw error;
